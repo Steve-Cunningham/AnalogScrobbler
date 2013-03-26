8c0724c6bf625df7cd8899297ddbef1f4d2580d8
Steve-Cunningham <steven.j.cunningham@gmail.com>
2013-03-25 23:22:47 -0500

    status as of Mar 25

--- js/scrobbler.js
+++ js/scrobbler.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // http://www.last.fm/api/auth?api_key=cd548a1b3639125edc41d192a1b69da5
 
     // Search by Artist/Album
 
@@ -32,30 +33,18 @@ $(document).ready(function(){
                 albumID = event.target.id;
                 $("#tracklist").append('<table class="table table-striped" id="songs"></table>');
                 $("#songs").html('');
+                $("#songs").html('<caption>Now Scrobbling "' + data.title + '"</caption>');
                 $("#songs").append('<tr><th id="scrobblecheck"><input type="checkbox" id="chkAll" /></th><th id="tabletrack">#</th><th id="tabletitle">Title</th><th id="tableartist">Artist</th><th id="tablelength">Length</th></tr>')
                 $("#songs").append('<button type="button" id="scrobblebutton" class="btn btn-danger">Scrobble</button>')
+                var trackScrobble = [];
                 $.getJSON("http://api.discogs.com/releases/" + albumID +"?callback=?", function(data) {
                     console.log(data);
                     $.each(data.data.tracklist, function () {
-                        $("#songs").append('<tr><td><input type="checkbox" class="trackselector"></td><td>' + this.position + '</td><td id="tracktitle">' + this.title + '</td><td ="trackartist">' + data.data.artists[0].name + '</td><td id="tracklength">' + this.duration + '</td>');
+                        $("#songs").append('<tr class="trackrow" id=' + albumID + this.position + '><td><input type="checkbox" class="trackselector" checked="checked"></td><td class="tracknumber">' + this.position + '</td><td class="tracktitle">' + this.title + '</td><td class="trackartist">' + data.data.artists[0].name + '</td><td class="tracklength">' + this.duration + '</td>');
                             $("#chkAll").click(function(){
                                 $(".trackselector").prop("checked",$("#chkAll").prop("checked"))
                             }); 
                     });
-
-
-                    // var newXHR = new XMLHttpRequest();
-
-                    trackScrobble = {
-                            'artist' : this.artist,
-                            'track' : this.title,
-                            'timestamp': null,
-                            'username' : username,
-                            'password' : password
-                    };
-
-
-
 
 
                     });  // end display table                    
@@ -65,17 +54,34 @@ $(document).ready(function(){
                     // begin scrobble function
 
                     $("#scrobblebutton").on('click', function () {
-                        console.log(trackScrobble);
+
+                            var submissionArray = [];
+                            var boxes = $(":checkbox:checked")
+
+                            $('#songs .trackrow').each(function() {
+                                var $this = $(this)
+
+                                var scrobbleTitle = $this.find('.tracktitle').html();
+                                var scrobbleArtist = $this.find('td.trackartist').html();
+                                var scrobbleTime = $this.find('td.tracklength').html();
+                                console.log(scrobbleTitle, scrobbleArtist, scrobbleTime);
+                                submissionArray.push(scrobbleTitle, scrobbleArtist, scrobbleTime);
+
+                            });
 
 
-                        $.ajax({
-                            url: 'http://post.audioscrobbler.com:80/',
-                            type: 'POST',
-                            dataType: 'jsonp',
-                            method: 'track.scrobble',
-                            data: trackScrobble,
-                            success: alert("Success")
-                        });
+                        // get token
+
+                        // get session ID
+
+
+                        // $.ajax({
+                        //     url: 'http://post.audioscrobbler.com:80/',
+                        //     type: 'POST',
+                        //     dataType: 'jsonp',
+                        //     method: 'track.scrobble',
+                        //     data: trackScrobble,
+                        // });
 
                         });  // end scrobblebutton
 
@@ -87,8 +93,6 @@ $(document).ready(function(){
                 
 
             }}     // end results tab generator
-
-
 
 
     );
